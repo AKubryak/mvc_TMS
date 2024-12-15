2a483a937b401fe45842ce1d5a336d0d4e039cf2
AKubryak <96373690+AKubryak@users.noreply.github.com>
2024-12-16 00:08:10 +0300

    TaskListView.php, HomeController.php, list.template.php

--- src/View/templates/list.template.php
+++ src/View/templates/list.template.php
@@ -1,2 +1,52 @@
 <?php
 
+use Model\Models\TaskModel;
+
+/**
+ * @var array<TaskModel> $tasks
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task list</title>
+</head>
+
+<body>
+    <form method="post" action="/update">
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Date</th>
+                <th></th>
+            </tr>
+            <?php foreach ($tasks as $task):
+
+                $background = 'white';
+
+                if ($task->status == 1) {
+                    $background = 'green';
+                } else if ($task->dueDate->getTimestamp() < time()) {
+                    $background = 'red';
+                }
+
+            ?>
+                <tr style="background-color: <?= $background ?>">
+                    <td><?= $task->id ?></td>
+                    <td><?= $task->name ?></td>
+                    <td><?= $task->description ?? '' ?></td>
+                    <td><?= $task->dueDate->format('d.m.Y H:i:s') ?></td>
+                    <td><input type="submit" value="completed" name="completed"></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </form>
+</body>
+
+</html>
